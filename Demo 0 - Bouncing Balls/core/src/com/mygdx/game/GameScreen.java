841df450e9bba9c7e210804d5909f84b10a9aea1
gerardfp <gerardfp@gmail.com>
2017-03-10 13:32:33 +0100

    Demo 0

--- Demo 0 - Bouncing Balls/core/src/com/mygdx/game/GameScreen.java
+++ Demo 0 - Bouncing Balls/core/src/com/mygdx/game/GameScreen.java
@@ -16,7 +16,7 @@ public class GameScreen extends MyGdxGameScreen {
 
     float newBallTimer;
     float gravity = -0.4f;
-    float spriteSize = 100;
+    float spriteSize = 50;
 
     public GameScreen(Game game) {
         super(game);
@@ -31,25 +31,24 @@ public class GameScreen extends MyGdxGameScreen {
         newBallTimer = 0;
     }
 
-    public void update(float delta){
+    @Override
+    public void render(float delta) {
+
+        // update
         newBallTimer += delta;
 
         for(Ball b : balls){
-            b.x += b.speedX * b.directionX;
-            b.y += b.speedY * b.directionY;
+            b.x += b.speedX;
+            b.y += b.speedY;
 
-            b.speedY += gravity * b.directionY;
+            b.speedY += gravity;
 
             if(b.x > Gdx.graphics.getWidth() - spriteSize || b.x < 0){
-                b.directionX *= -1;
+                b.speedX *= -1;
             }
 
             if(b.y < 0){
                 b.speedY = b.reboundSpeed;
-            }
-
-            if(b.y < 0 || b.speedY <= 0){
-                b.directionY *= -1;
             }
         }
 
@@ -57,12 +56,8 @@ public class GameScreen extends MyGdxGameScreen {
             balls.add(new Ball());
             newBallTimer = 0;
         }
-    }
 
-    @Override
-    public void render(float delta) {
-        update(delta);
-
+        // render
         Gdx.gl.glClearColor(0.52f, 0.80f, 0.87f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
